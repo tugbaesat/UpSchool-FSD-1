8219a2ca3c53c41d6351130473d07fcfeaa69e27
tugbaesat <tugbaesat007@gmail.com>
2023-02-25 19:37:27 +0300

    Create HW1-Password-Generator

---
+++ HW1-Password-Generator/Generator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1passwordGenerator.Console
+{
+  
+    public class Generator
+
+    {
+        public static string GeneratePassword(bool useUpper,
+        bool useLower,
+        bool useNumbers,
+        bool useSpecialChars,
+        int length)
+        {
+            string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+            string numbers = "0123456789";
+            string specialChars = "!@#$%^&*()";
+
+            Random random = new Random(); 
+            string charSet = string.Empty;
+            char[] password = new char[length];
+        
+            if (useUpper) charSet += upperCase;
+            if (useLower)  charSet += lowerCase;
+            if (useNumbers) charSet += numbers;
+            if (useSpecialChars) charSet += specialChars;
+
+            for (int i = 0; i < length; i++)
+            {
+                password[i] = charSet[random.Next(charSet.Length -1)];
+            }
+            return string.Join(null, password);
+        }
+    }
+}
---
+++ HW1-Password-Generator/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using hw1passwordGenerator.Console;
+
+Console.WriteLine("How long do you want your password to be?");
+int passwordLength =Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Do you want to include lowercase characters? (y / n)");
+var lowerChar = Console.ReadLine();
+bool isLowerChar = false;
+
+Console.WriteLine("Do you want to include uppercase characters? (y / n)");
+var upperChar = Console.ReadLine();
+bool isUpperChar = false;
+
+Console.WriteLine("Do you want to include numbers? (y / n)");
+var numChar = Console.ReadLine();
+bool isNumChar = false;
+
+Console.WriteLine("Do you want to include special characters? (y / n)");
+var specialChar = Console.ReadLine();
+bool isSpecialChar = false;
+
+
+isLowerChar = lowerChar == "y" || lowerChar == "Y";
+
+isUpperChar = upperChar == "y" || upperChar == "Y";
+
+isNumChar = numChar == "y" || numChar == "Y";
+
+isSpecialChar = specialChar == "y" || specialChar == "Y";
+
+if (!isLowerChar && !isUpperChar && !isNumChar && !isSpecialChar || passwordLength == 0)
+{
+    Console.WriteLine("I can't generate a password with no characters:(.");
+}
+else
+{
+    Console.WriteLine(Generator.GeneratePassword(isUpperChar, isLowerChar, isNumChar, isSpecialChar, passwordLength));
+}
+
+
+
+
